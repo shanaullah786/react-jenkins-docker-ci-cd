60670022efc76fa7453e3a7b126a3a939bf7087b
Iphtekhar K <iphtekhar.k@publicissapient.com>
2021-06-28 23:39:12 +0530

    unit test added

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          React Jenkins Docker CD/CD Pipeline Demo
         </p>
         <a
           className="App-link"
